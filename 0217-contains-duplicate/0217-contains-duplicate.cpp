4d21e4e132d286f197321e96ac4b9ddbe951febf
Atharva Khaire <163679944+sLasherGod10@users.noreply.github.com>
2026-06-16 13:23:15 +0530

    Time: 55 ms (32.18%), Space: 92.8 MB (17.63%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st(nums.begin() , nums.end());
+
+        return st.size() != nums.size();
+        
+    }
+};
