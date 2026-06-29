0bd1e741fe18fa2d078fb24b847c4bdb205e7575
Atharva Khaire <163679944+sLasherGod10@users.noreply.github.com>
2026-06-30 00:15:55 +0530

    Time: 0 ms (100%), Space: 8.5 MB (86.97%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int i = 0;
-
-        while((long long)i * i <= x){
-            i++;
-        }
-        return i-1;
+        return sqrt(x);
+        
     }
 };
