f5a0d69495a43f156cf5a4bedcbd76a18e29a209
Atharva Khaire <163679944+sLasherGod10@users.noreply.github.com>
2026-08-24 00:09:44 +0530

    Time: 0 ms (100%), Space: 11.9 MB (58.13%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix =strs[0];
+        for(int i=0 ; i < strs.size() ; i++){
+            int j = 0;
+
+            while( j < prefix.length() && j < strs[i].length() && prefix[j] == strs[i][j]){
+                j++;
+            }
+            prefix = prefix.substr(0, j);
+
+            // No common prefix
+            if (prefix.empty()) {
+                return "";
+            }
+        }
+        return prefix;
+        
+    }
+};
